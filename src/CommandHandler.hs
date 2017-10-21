27a97b8deaac840eafba8ba0b6b5f80f73cd06d1
bnjhope <bnjhope1@gmail.com>
2017-10-21 18:03:03 +0100

    add filename handler

--- src/CommandHandler.hs
+++ src/CommandHandler.hs
@@ -1,13 +1,28 @@
 module CommandHandler
-( handleInput
+( ProgramInfo,
+handleInput
 ) where
 
 import System.Environment
+
+data ProgramInfo = ProgramInfo {
+	filename :: String
+	, args :: [String]
+}
 
 handleInput :: IO()
 handleInput = do
 	args <- getArgs
 	
+getProgramInfo :: [String] -> ProgramInfo
+getProgramInfo xs = 
+
+getArgsAndFilename :: [String] -> ([String], [String])
+getArgsAndFilename args :: partition $ argIsFlag args
+
+partitionTupleToProgramInfo :: ([String], [String]) -> ProgramInfo
+partitionTupleToProgramInfo ([flags], [filenameList]) = ProgramInfo (head filenameList) flags
+
 getFlagsFromArgs :: [String] -> [String]
 getFlagsFromArgs args = filter argIsFlag args
 
