108ba3001c9fcc3272b51f36b327037216e5f0a0
Ben Hope <ben.hope@skyscanner.net>
2017-10-25 07:40:03 +0100

    add constant as a type of expression

--- src/Structures.hs
+++ src/Structures.hs
@@ -5,6 +5,10 @@ Constant(..)
 
 type AST = Constant
 
+-- | The expression type
+data Expr = EConstant Constant
+
+-- | Data type for a constant
 data Constant = EInt Integer
     | EFloat Float
     | EBoolean Bool
