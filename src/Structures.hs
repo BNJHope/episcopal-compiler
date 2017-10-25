545942823b5a7401d2e76a2e87fd9b5972f60c66
Ben Hope <ben.hope@skyscanner.net>
2017-10-25 21:32:07 +0100

    add program and query structures

--- src/Structures.hs
+++ src/Structures.hs
@@ -1,21 +1,42 @@
 module Structures(
 AST,
+ID,
+Program(..),
+Query(..),
 Expression(..),
 Constant(..),
-Expr(..)
+Expr(..),
+Args
 ) where
 
 type AST = Expression
+type ID = String
 
 data Expression = EExpr Expr
     | EConstant Constant
+    | EProgram Program
+    | EQuery Query
+    | EArgs Args
+
+data Program = Program {
+programId :: ID,
+programExpr :: Expr, 
+programQueries :: [Query]
+}
+
+data Query = Query {
+queryId :: ID,
+queryArgs :: Args,
+queryExprs :: [Expr]
+}
 
 -- | The expression type
 data Expr = ExprConstant Constant
 
 -- | Data type for a constant
-data Constant = EInt Integer
+data Constant = EInt Int
     | EFloat Float
     | EBoolean Bool
     | EPercentage Float
 
+type Args = [ID]
