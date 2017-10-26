6a3f91348200c08c4a1c76315a64f712491e2fc6
bnjhope <bnjhope1@gmail.com>
2017-10-26 16:30:05 +0100

    add all structures

--- src/Structures.hs
+++ src/Structures.hs
@@ -18,20 +18,30 @@ data Expression = EExpr Expr
     | EQuery Query
     | EArgs Args
 
+-- | Program Structure
 data Program = Program {
-programId :: ID,
-programExpr :: Expr, 
-programQueries :: [Query]
+    programId :: ID,
+    programExpr :: Expr, 
+    programQueries :: [Query]
 }
 
+-- | Query Structure
 data Query = Query {
-queryId :: ID,
-queryArgs :: Args,
-queryExprs :: [Expr]
+    queryId :: ID,
+    queryArgs :: Args,
+    queryExprs :: [Expr]
 }
 
 -- | The expression type
 data Expr = ExprConstant Constant
+    | ExprDef [Definition] Expr
+    | ExprObserve Expr Expr
+    | ExprSample Expr
+    | ExprDistrib Distribution
+    | ExprID ID
+    | ExprFunctionCall ID [Expr]
+    | ExprBinOp Expr Op Expr
+    | ExprBracketing Expr
 
 -- | Data type for a constant
 data Constant = EInt Int
@@ -39,4 +49,26 @@ data Constant = EInt Int
     | EBoolean Bool
     | EPercentage Float
 
+-- | Definition structure
+data Definition = FuncDef ID Args [Expr]
+    | DistDef ID Args [Expr]
+
 type Args = [ID]
+
+-- | Set of distributions
+data Distribution = DistBernoulli Expr
+    | DistBeta Expr Expr
+    | DistNormal Expr Expr
+    | DistFlip Expr
+    | DistUserDefined [Expr]
+
+-- | Set of binary operations.
+data Op = ADD
+    | MULT
+    | SUBTRACT
+	| OVER
+	| OR
+	| AND
+	| GREATER_THAN
+	| LESS_THAN
+	| EQUALS
