6caf3dd0b712eb23bd02c1c5a8da2e24c2af3291
bnjhope <bnjhope1@gmail.com>
2017-11-01 13:31:26 +0000

    print steps

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
+BLUE='\033[0;34m'
+NC='\033[0m'
+echo -e "${BLUE}--- BUILDING COMPILER ---${NC}\n"
 cabal clean; cabal build
+echo -e "\n${BLUE}--- COMPILING ASTS INTO output_progs ---${NC}\n"
 ./dist/build/pldi-p1/pldi-p1
