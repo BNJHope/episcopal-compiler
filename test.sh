1b6a684647fca293e084ca2d5ec8e04b66b07e8d
bnjhope <bnjhope1@gmail.com>
2017-11-01 13:43:06 +0000

    use cabal run command instead of directly executing binary

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ NC='\033[0m'
 echo -e "${BLUE}--- BUILDING COMPILER ---${NC}\n"
 cabal clean; cabal build
 echo -e "\n${BLUE}--- COMPILING ASTS INTO output_progs ---${NC}\n"
-./dist/build/pldi-p1/pldi-p1
+cabal run
