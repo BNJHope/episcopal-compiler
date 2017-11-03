c1d3644cd7dec508baa9c4a39560598293095721
bnjhope <bnjhope1@gmail.com>
2017-11-03 16:10:34 +0000

    add more test ASTs

--- src/TestAST.hs
+++ src/TestAST.hs
@@ -6,34 +6,58 @@ getTestASTs
 import Structures
 
 getTestASTs :: [Program]
-getTestASTs = [getTestAST1,
-    getTestAST2,
-    getTestAST3,
-    getTestAST4,
-    getTestAST5,
-    getTestAST6,
-    getTestAST7]
+getTestASTs = [ast1,
+    ast2,
+    ast3,
+    ast4,
+    ast5,
+    ast6,
+    ast7,
+    ast8,
+    ast9,
+    ast10,
+    ast11,
+    ast12,
+    ast13]
 
-getTestAST1 :: Program
-getTestAST1 = Program "TestConst" constant1 []
+ast1 :: Program
+ast1 = Program "TestConst" constant1 []
 
-getTestAST2 :: Program
-getTestAST2 = Program "TestFunc" func1 []
+ast2 :: Program
+ast2 = Program "TestFunc" func1 []
 
-getTestAST3 :: Program
-getTestAST3 = Program "TestNestedFunc" func2 []
+ast3 :: Program
+ast3 = Program "TestNestedFunc" func2 []
 
-getTestAST4 :: Program
-getTestAST4 = Program "TestQuery" (funcCallSum constant42 constant42) [querySum]
+ast4 :: Program
+ast4 = Program "TestQuery" (funcCallSum constant42 constant42) [querySum]
 
-getTestAST5 :: Program
-getTestAST5 = Program "TestNestedFuncInQuery" (funcCallIncrement constant42) [queryIncrement]
+ast5 :: Program
+ast5 = Program "TestNestedFuncInQuery" (funcCallIncrement constant42) [queryIncrement]
 
-getTestAST6 :: Program
-getTestAST6 = Program "TestDistribution" exprBernoulliHalf []
+ast6 :: Program
+ast6 = Program "TestBernoulli" exprBernoulliHalf []
 
-getTestAST7 :: Program
-getTestAST7 = Program "TestSampleBernoulli" (ExprSample exprBernoulliHalf) []
+ast7 :: Program
+ast7 = Program "TestSampleBernoulli" (ExprSample exprBernoulliHalf) []
+
+ast8 :: Program
+ast8 = Program "TestBeta" exprBetaFiveTwelve []
+
+ast9 :: Program
+ast9 = Program "TestSampleBeta" (ExprSample exprBetaFiveTwelve) []
+
+ast10 :: Program
+ast10 = Program "TestNormal" exprNormalFiveHalve []
+
+ast11 :: Program
+ast11 = Program "TestSampleNormal" (ExprSample exprNormalFiveHalve) []
+
+ast12 :: Program
+ast12 = Program "TestFlip" exprFlipHalf []
+
+ast13 :: Program
+ast13 = Program "TestSampleFlip" (ExprSample exprFlipHalf) []
 
 func1 :: Expr
 func1 = ExprDef [funcDefSum] (funcCallSum constant42 constant42)
@@ -65,8 +89,26 @@ funcCallIncrement expr1 = ExprFunctionCall "increment" [expr1]
 exprBernoulliHalf :: Expr
 exprBernoulliHalf = (ExprDistrib $ bernoulli constantHalf)
 
+exprBetaFiveTwelve :: Expr
+exprBetaFiveTwelve = (ExprDistrib $ beta constantFive constantTwelve)
+
+exprNormalFiveHalve :: Expr
+exprNormalFiveHalve = (ExprDistrib $ normal constantFive constantHalf)
+
+exprFlipHalf :: Expr
+exprFlipHalf = (ExprDistrib $ flipDist constantHalf)
+
 bernoulli :: Expr -> Distribution
 bernoulli p = DistBernoulli p
+
+beta :: Expr -> Expr -> Distribution
+beta alpha beta = DistBeta alpha beta
+
+normal :: Expr -> Expr -> Distribution
+normal mean stanDev = DistNormal mean stanDev
+
+flipDist :: Expr -> Distribution
+flipDist p = DistFlip p
 
 constant1 :: Expr
 constant1 = ExprConstant $ EInt 1
@@ -76,3 +118,9 @@ constant42 = ExprConstant $ EInt 42
 
 constantHalf :: Expr
 constantHalf = ExprConstant $ EFloat 0.5
+
+constantFive :: Expr
+constantFive = ExprConstant $ EInt 5
+
+constantTwelve :: Expr
+constantTwelve = ExprConstant $ EInt 12
